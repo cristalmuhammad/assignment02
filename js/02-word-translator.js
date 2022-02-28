116834234e7c8baf80bac354827384846be6b07d
Cristal Muhammad <cristalmuhammad@gmail.com>
2022-02-27 17:56:40 -0800

    init commit

---
+++ js/02-word-translator.js
@@ -0,0 +1,20 @@
+// Holla Mundo! es
+// Bonjour le monde! fr
+// Hallo Welt! de
+// Hello World! en
+
+let languageCode = prompt('Enter the language code of your choice')
+let es = ('Holla Mundo!')
+let en = ('Hello World')
+if (es) {
+    document.write(es) }
+if (en) {
+    document.write(en) }
+if (fr) {
+    document.write('Bonjour le Monde!') }
+if (de) {
+    document.write('Hallo Welt!')
+}
+else {
+    document.write('Hello World')
+}
