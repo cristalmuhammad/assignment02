03fce77b714a413ab21d54b65b98889d1c6187e0
Cristal Muhammad <cristalmuhammad@gmail.com>
2022-02-27 18:11:15 -0800

    initial commit

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,11 +1,11 @@
-// let num1 = parseInt(prompt('Enter a number between 1 and 100'))
-// let num2 = parseInt(prompt('Enter another number between 1 and 100'))
-//  if (num1 > num2 ) {
-//      document.write(num1)
-//  }
-//   if (num2 > num1) {
-//       document.write(num2)
-//   } 
-//   if (num1 === num2) {
-//       alert('You must choose a different number')
-//   }
+let num1 = parseInt(prompt('Enter a number between 1 and 100'))
+let num2 = parseInt(prompt('Enter another number between 1 and 100'))
+ if (num1 > num2 ) {
+     document.write(num1)
+ }
+  if (num2 > num1) {
+      document.write(num2)
+  } 
+  if (num1 === num2) {
+      alert('You must enter a different number')
+  }
